c12659f430aaf0cc0d682e122d093eff6916144a
Mike Mullen <m@m-mullen.com>
2014-02-07 10:26:05 -0800

    Finish v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Mullenlabs::Application.routes.draw do
   get "static_pages/cases"
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/krav-maga-extreme', to: 'static_pages#krav-maga-extreme', via: 'get'
+  match '/residential', to: 'static_pages#residential', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
